35e0d04db10d5c6de38b673fece2d2915db14db7
Lianderson <liandersonr18@gmail.com>
2025-04-22 13:25:16 -0300

    feat: colocando as rotas principais

--- app.js
+++ app.js
@@ -1,13 +1,59 @@
 import express from "express"
 const app = express()
 const port = 3000
+// Permitir let Json no corpo da requisição
+app.use(express.json());
+
+
+//Banco de dados Fake (em memória)
+const usuarios=[
+  {id:1,nome:"Jonas",email:"jonas@gmail.com"},
+  {id:2,nome:"Ana",email:"ana@gmail.com"}
+];
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Bem vindo a minha API!');
 })
 
-app.get('/users', (req, res) => {
-    res.send([{nome:"valdiano"}]);
+app.get('/usuarios', (req, res) => {
+  res.send(usuarios);
+})
+app.post('/criarUsuario', (req, res)=>{
+  try {
+    const {nome,email} = req.body;
+    const ultimoUsuario = usuarios.slice(-1);
+    const proximoId=ultimoUsuario[0].id+1;
+    usuarios.push({id:proximoId,nome:nome,email:email})
+    res.send(`usuario ${nome} foi adicionado`);
+    console.log("adicionado com sucesso");
+  } catch (error) {
+    console.log(`erro ao adicionar o usuario ${error.message}`);
+  }
+})
+
+app.put("/usuario/id=:id",(req,res)=>{
+  const {id} = req.params;
+  const {novoNome,novoEmail}= req.body;
+  const usuario = usuarios.find((usuario)=>usuario.id==id)
+  if(usuario){
+  usuario.nome=novoNome;
+  usuario.email=novoEmail;
+  res.send("usuário atualiizado");
+  }else{
+    res.send("usuário não encontrado");  
+  }
+})
+
+app.delete("/deletarUsuario/id=:id",(req,res)=>{
+  const {id}= req.params
+  const usuario = usuarios.find((usuario)=>usuario.id==id)
+  if(usuario){
+  const indexUsuario=usuarios.indexOf(usuario);
+  usuarios.splice(indexUsuario,1)
+  res.send("usuario apagado com sucesso");
+  }else{
+    res.send("usuario não encontrado");
+  }
 })
   
 
